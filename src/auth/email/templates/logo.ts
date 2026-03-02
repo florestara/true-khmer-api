1828958ba12866555e412a88abf4a6f51191cb16
thisisnotleng <nsleng168@gmail.com>
2026-03-02 13:33:03 +0700

    fix: added logo image for email template.

--- src/auth/email/templates/logo.ts
+++ src/auth/email/templates/logo.ts
@@ -1,7 +1,7 @@
 // actual img link will be replaced after R2 Implemented.
 
-export const trueKhmerLogo = 'link-to-true-khmer-logo';
-export const otpLogo = 'link-to-otp-logo';
-export const igLogo = 'link-to-instagram-logo';
-export const threadLogo = 'link-to-threads-logo';
-export const xLogo = 'link-to-x-logo';
+export const trueKhmerLogo = 'https://pub-43fcf325b98b4cdda61e323d4b4a26c4.r2.dev/email-template/TK%20Main%20Logo.png';
+export const otpLogo = 'https://pub-43fcf325b98b4cdda61e323d4b4a26c4.r2.dev/email-template/asterisk-hexagon.png';
+export const igLogo = 'https://pub-43fcf325b98b4cdda61e323d4b4a26c4.r2.dev/email-template/brand-instagram.png';
+export const threadLogo = 'https://pub-43fcf325b98b4cdda61e323d4b4a26c4.r2.dev/email-template/brand-threads.png';
+export const xLogo = 'https://pub-43fcf325b98b4cdda61e323d4b4a26c4.r2.dev/email-template/brand-x.png';
